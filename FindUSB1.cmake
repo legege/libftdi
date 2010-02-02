5478cd5944da978707c0c23c5c7a84554a0ea6f7
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2010-02-02 18:52:37 +0100

    We need to find libusb-1, so adapt the CMake support files

--- FindUSB1.cmake
+++ FindUSB1.cmake
@@ -21,13 +21,13 @@ else (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
     # use pkg-config to get the directories and then use these values
     # in the FIND_PATH() and FIND_LIBRARY() calls
     find_package(PkgConfig)
-    pkg_check_modules(PC_LIBUSB libusb)
+    pkg_check_modules(PC_LIBUSB libusb-1.0)
   ENDIF(NOT WIN32)
 
-  FIND_PATH(LIBUSB_INCLUDE_DIR usb.h
+  FIND_PATH(LIBUSB_INCLUDE_DIR libusb.h
     PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
 
-  FIND_LIBRARY(LIBUSB_LIBRARIES NAMES usb 
+  FIND_LIBRARY(LIBUSB_LIBRARIES NAMES usb-1.0
     PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
 
   include(FindPackageHandleStandardArgs)
