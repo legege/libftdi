35ac57123b3493bcd43c9920e5517ddda1de8e3d
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2011-07-05 18:03:48 +0200

    examples/find-all: Use the new "find-all" option

--- examples/find_all.c
+++ examples/find_all.c
@@ -23,7 +23,7 @@ int main(void)
         return EXIT_FAILURE;
     }
 
-    if ((ret = ftdi_usb_find_all(&ftdic, &devlist, NULL, NULL)) < 0)
+    if ((ret = ftdi_usb_find_all(&ftdic, &devlist, 0, 0)) < 0)
     {
         fprintf(stderr, "ftdi_usb_find_all failed: %d (%s)\n", ret, ftdi_get_error_string(&ftdic));
         retval =  EXIT_FAILURE;
