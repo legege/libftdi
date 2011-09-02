a87a0712f7166d3418a7b522b44ffbce97a93d50
Thomas Jarosch <thomas.jarosch@intra2net.com>
2011-09-02 11:40:41 +0200

    Add (optional) unit test infrastructure
    Requires unit test framework from boost
    so we can test the C and C++ code.
    
    Also has automatic test registration
    and the ability to run tests individually.

---
+++ test/baudrate.cpp
@@ -0,0 +1,24 @@
+/**@file
+@brief Test baudrate calculator code
+
+@author Thomas Jarosch
+*/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License           *
+ *   version 2.1 as published by the Free Software Foundation;             *
+ *                                                                         *
+ ***************************************************************************/
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(Baudrate)
+
+BOOST_AUTO_TEST_CASE(Simple)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
