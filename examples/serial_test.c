2e9cc5be4eb2115cdb8a497d6b9def031b1a681e
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2011-09-05 11:40:07 +0200

    examples/serial_test.c: Use FTDI vendor ID as default

--- examples/serial_test.c
+++ examples/serial_test.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     struct ftdi_context *ftdi;
     unsigned char buf[1024];
     int f, i;
-    int vid = 0;
+    int vid = 0x403;
     int pid = 0;
     int baudrate = 115200;
     int interface = INTERFACE_ANY;
