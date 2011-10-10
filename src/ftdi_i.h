720c7d2457ae1cd43abd36b3ac7e89b896e65119
Thomas Jarosch <thomas.jarosch@intra2net.com>
2011-10-10 08:19:05 +0200

    Increase ftdi_eeprom->cbus_function array again

--- src/ftdi_i.h
+++ src/ftdi_i.h
@@ -79,7 +79,7 @@ struct ftdi_eeprom
 
     /* Special function of FT232R/FT232H devices (and possibly others as well) */
     /** CBUS pin function. See CBUS_xxx defines. */
-    int cbus_function[9];
+    int cbus_function[10];
     /** Select hight current drive on R devices. */
     int high_current;
     /** Select hight current drive on A channel (2232C */
